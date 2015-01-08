1d6d7a161480ec5d4d12e9d4f0a0bfa023eac783
Todd Bashor <jadedsurfer@gmail.com>
2015-01-08 15:36:41 -0800

    Clean up sample integration test after demo

--- neukinnis/spec/integration/sample-integration.spec.js
+++ neukinnis/spec/integration/sample-integration.spec.js
@@ -5,8 +5,8 @@ casper.test.begin('page 1 navigates to page 2', 3, function suite(test){
   var link = 'a[href=\'/page2.html\']';
 
   casper.start('http://localhost:3000/page1.html', function(){
-    test.assertTitle('Page 1', 'title is the one expected');
-    test.assertExists(link, 'link to foo is found');
+    test.assertTitle('Page 1', 'page 1 title is the one expected');
+    test.assertExists(link, 'link to page 2 is found');
   });
 
   casper.then(function(){
@@ -14,7 +14,7 @@ casper.test.begin('page 1 navigates to page 2', 3, function suite(test){
   });
 
   casper.then(function(){
-    test.assertTitle('Page 2', 'bar title is the one expected');
+    test.assertTitle('Page 2', 'page 2 title is the one expected');
   });
 
   casper.run(function(){
