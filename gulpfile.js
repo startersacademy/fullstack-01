7816f1c163a060758bbd6601bc55d131602adaaa
jstadragon <jstadragon@gmail.com>
2015-01-10 01:17:36 -0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var spawn = require('child_process').spawn,
+    gulp = require('gulp'),
+    gutil = require('gulp-util');
+
+gulp.task('integrate', function () {
+    var tests = ['./spec/integration'];
+
+    var casperChild = spawn('casperjs.cmd', ['test'].concat(tests));
+
+    casperChild.stdout.on('data', function (data) {
+        gutil.log('CasperJS:', data.toString().slice(0, -1)); // Remove \n
+    });
+
+    casperChild.on('close', function (code) {
+        var success = code === 0; // Will be 1 in the event of failure
+
+        // Do something with success here
+    });
+});
