11cc9effce5fcee5fc0422c71154fe16e12a3a98
Todd Bashor <jadedsurfer@gmail.com>
2015-01-10 01:26:52 -0800

    Use gulp-develop-server to start the server before running the casper tests

--- neukinnis/gulpfile.js
+++ neukinnis/gulpfile.js
@@ -1,19 +1,24 @@
 var gulp = require('gulp');
 var casperJs = require('gulp-casperjs');
 var run = require ('gulp-run');
+var server = require('gulp-develop-server');
 
-gulp.task('start-server', function(){
-  console.log('starting server');
-  var startServer = new run.Command('node .');
-  startServer.exec();
+//gulp.task('server:start', function(){
+//  console.log('starting server');
+//  var startServer = new run.Command('node .');
+//  startServer.exec();
+//});
+
+gulp.task('server:start', function(){
+  server.listen({path: '.'});
 });
 
-gulp.task('integrate', ['start-server'], function () {
+gulp.task('integrate', ['server:start'], function () {
   console.log('starting to run integration tests');
-  setTimeout(function(){
+  //setTimeout(function(){
     gulp.src('./spec/integration/**')
       .pipe(casperJs({command:'test'}));
-  }, 3000);
+  //}, 3000);
 
 });
 
