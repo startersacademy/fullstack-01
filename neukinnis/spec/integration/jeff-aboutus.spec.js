e5fcafe9d5038736aca29fbe845dc16da415507e
jstadragon <jstadragon@gmail.com>
2015-01-12 12:20:06 -0800

    rewrite output messages

--- neukinnis/spec/integration/jeff-aboutus.spec.js
+++ neukinnis/spec/integration/jeff-aboutus.spec.js
@@ -4,56 +4,36 @@
 
 casper.test.begin('About us page test', 3,
   function suite(test){
-    casper.start('http://localhost/neukinnis/client/aboutus.html', function(){
-    test.assertTitle('About Us', 'open about us success');
-    test.assertExists('a[href="index.html"]', 'found link home');
+    casper.start('http://localhost/neukinnis/client/index.html', function(){
+    test.assertTitle('Neukinnis', 'Finds the main page with title: Neukinnis');
+    test.assertExists('a[href="aboutus.html"]', 'Checks existence link to about us page');
   });
   casper.then(function(){
-    this.click('a[href="index.html"]');
+    this.click('a[href="aboutus.html"]', 'Navigates and clicks on link to about us page');
   });
   casper.then(function(){
-    test.assertTitle('Neukinnis', 'open home success');
-  });
-  casper.thenOpen('http://localhost/neukinnis/client/aboutus.html').then(function(){
-    test.assertTitle('About Us', 'return to about us');
-    test.assertExists('a[href="privacy.html"]', 'found link privacy');
+    test.assertTitle('About Us', 'Finds page title About Us');
+    test.assertExists('a[href="privacy.html"]', 'Check existence of link to privacy page');
   });
   casper.then(function(){
-    this.click('a[href="privacy.html"]');
+    this.click('a[href="privacy.html"]', "Navigates and clicks on link to privage page");
   });
   casper.then(function(){
-    test.assertTitle('Privacy', 'open privacy success');
-  });
-  casper.thenOpen('http://localhost/neukinnis/client/aboutus.html').then(function(){
-    test.assertTitle('About Us', 'return to about us');
-    test.assertExists('a[href="contactus.html"]', 'found link contactus');
+    test.assertTitle('Privacy', 'Finds page title Privacy');
+    test.assertExists('a[href="index.html"]', 'Check existence of link to home page');
   });
   casper.then(function(){
-    this.click('a[href="contactus.html"]');
+    this.click('a[href="index.html"]', "Navigates and clicks on link to home page");
   });
-  casper.then(function(){
-    test.assertTitle('Contact Us', 'open contact us success');
+    casper.then(function(){
+    test.assertTitle('Neukinnis', 'Finds page title Neukinnis');
+    test.assertExists('a[href="courses.html"]', 'Check existence of link to courses page');
   });
-  casper.thenOpen('http://localhost/neukinnis/client/aboutus.html').then(function(){
-    test.assertTitle('About Us', 'return to about us');
-    test.assertExists('a[href="courses.html"]', 'found link courses');
+    casper.then(function(){
+    this.click('a[href="courses.html"]', "Navigates and clicks on link to courses page");
   });
-  casper.then(function(){
-    this.click('a[href="courses.html"]');
-  });
-  casper.then(function(){
-    test.assertTitle('Courses', 'open courses success');
-  });
-  casper.thenOpen('http://localhost/neukinnis/client/aboutus.html').then(function(){
-    test.assertTitle('About Us', 'return to about us');
-    test.assertExists('a[href="instructors.html"]', 'found instructors link');
-  });
-  casper.then(function(){
-    this.click('a[href="instructors.html"]');
-  });
-  casper.then(function(){
-    test.assertTitle('Instructors', 'open instructors success');
-  });
+    casper.then(function(){
+    test.assertTitle('Courses', 'Finds page title Courses');
   casper.run(function(){
     test.done();
   });
