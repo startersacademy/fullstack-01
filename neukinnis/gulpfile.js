6814b71777cf2af61d3095d433bc524c04fe4a4d
frances.go <goldlilys@goldlilys-media.com>
2015-01-12 20:52:09 -0800

    Add gulpfile to automate integration test

---
+++ neukinnis/gulpfile.js
@@ -0,0 +1,9 @@
+'use strict'; 
+
+var gulp = require('gulp');
+var casperjs = require('gulp-casperjs');
+
+gulp.task('integrate', function(){
+    gulp.src('./spec/integration/**')
+        .pipe(casperjs({command:'test'}));
+});
