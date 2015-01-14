625610f92a1ba862628b7dd4c97fe0bba614b151
Russia Madden <dataengineer007@gmail.com>
2015-01-13 19:14:59 -0800

    Add updates to html and test file

--- neukinnis/client/contact/spec/integration/test-name.spec.js
+++ neukinnis/client/contact/spec/integration/test-name.spec.js
@@ -1,20 +1,29 @@
 //test-name.spec.js
+/*
+Acceptance Criteria:
+I can:
+-Navigate to this page according to the site map url
+-See that the page looks like the mockup
+-View the page at different resolutions and the content is laid out logically
+*/
 
 /* global casper */
 
-casper.test.begin('page 1 navigates to page 2', 3,
+
+casper.test.begin('contact page navigates to home page ', 3,
   function suite(test){
   });
 
-casper.start('http://localhost:3000/contact.html', function(){
-  test.assertTitle('Page 1', 'page 1 title good');
+//casper.start('http://localhost:3000/contact.html', function(){
+casper.start('http://localhost:63342/fullstack-project-01/neukinnis/client/contact/contact.html', function(){
+  test.assertTitle('Contact Us', 'contact us page  title good');
   test.assertExists('.testedlink', 'link found');
 });
 casper.then(function(){
   this.click('.testedlink');
 });
 casper.then(function(){
-  test.assertTitle('Page 2', 'page 2 title good');
+  test.assertTitle('Home', 'home page title good');
 });
 casper.run(function(){
   test.done();
