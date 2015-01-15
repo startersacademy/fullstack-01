995f47bf17eed5dc8c696843681bf9c8209d3de5
Todd Bashor <jadedsurfer@gmail.com>
2015-01-15 09:41:41 -0800

    Enhance gulpfile to automate api testing

--- neukinnis/gulpfile.js
+++ neukinnis/gulpfile.js
@@ -2,28 +2,51 @@
 
 var gulp = require('gulp');
 var casperJs = require('gulp-casperjs');
-//var run = require ('gulp-run');
+var run = require ('gulp-run');
 var server = require('gulp-develop-server');
+var gulpSequence = require('gulp-sequence');
+//var jasmine = require('gulp-jasmine');
 
-//gulp.task('server:start', function(){
-//  console.log('starting server');
-//  var startServer = new run.Command('node .');
-//  startServer.exec();
-//});
 
+// Starts server for tasks that require a server
 gulp.task('server:start', function(cb){
   server.listen({path: '.', delay: 3000}, cb);
 });
 
-gulp.task('integrate', ['server:start'], function (err) {
-  console.log('starting to run integration tests');
-  var casper = casperJs({command:'test'});
-  casper.on('data', function(){
-  });
-  gulp.src('./spec/integration/**')
-      .pipe(casper).on('end', function() {
-      server.kill();
-    });
+// Stops server after tasks have run
+gulp.task('server:stop', function(cb){
+  server.kill();
+});
+
+
+// Runs integration tests with casperjs and phantomjs
+gulp.task('test:integration', function (cb) {
+  console.log('running integration tests');
+
+  var integrationTestGlob = './spec/integration/**';
+
+  gulp.src(integrationTestGlob)
+    .pipe(casperJs({command:'test'}))
+    .on('data', function(){})
+    .on('end', cb)
+    .on('error', cb);
 
 });
+
+// Runs api tests with frisbyjs and jasmine-node
+gulp.task('test:api', function (cb) {
+  console.log('running api tests');
+
+  var apiTestDir = 'spec/api/';
+
+  gulp.src(apiTestDir)
+    .pipe(run('jasmine-node ' + apiTestDir))
+      .on('data', function(){})
+      .on('end', cb)
+      .on('error', cb);
+
+});
+
+gulp.task('test:all',
+  gulpSequence('server:start', 'test:api', 'test:integration', 'server:stop'));
 
