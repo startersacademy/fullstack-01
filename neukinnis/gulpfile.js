faddf21e455d5773388d484c7d1c341eca53f399
jstadragon <jstadragon@gmail.com>
2015-01-15 12:20:52 -0800

    fix indentation issues

--- neukinnis/gulpfile.js
+++ neukinnis/gulpfile.js
@@ -1,17 +1,16 @@
 var spawn = require('child_process').spawn,
-    gulp = require('gulp'),
-    gutil = require('gulp-util');
+  gulp = require('gulp'),
+  gutil = require('gulp-util');
 
 gulp.task('integrate', function () {
-    var tests = ['./spec/integration'];
-    var casperChild = spawn('casperjs.cmd', ['test'].concat(tests));
+  var tests = ['./spec/integration'];
+  var casperChild = spawn('casperjs.cmd', ['test'].concat(tests));
+  casperChild.stdout.on('data', function (data) {
+    gutil.log('CasperJS:', data.toString().slice(0, -1)); // Remove \n
+  });
 
-    casperChild.stdout.on('data', function (data) {
-        gutil.log('CasperJS:', data.toString().slice(0, -1)); // Remove \n
-    });
-
-    casperChild.on('close', function (code) {
-        var success = code === 0; // Will be 1 in the event of failure
-        console.log("Test has finished!");
-    });
+  casperChild.on('close', function (code) {
+    var success = code === 0; // Will be 1 in the event of failure
+    console.log("Test has finished!");
+  });
 });
