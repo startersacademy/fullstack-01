17e285cdebb515c770c5c5de849698d95524af1c
Trey Hunner <trey@treyhunner.com>
2015-01-14 19:30:12 -0800

    Apply EditorConfig settings to gulpfile

--- neukinnis/gulpfile.js
+++ neukinnis/gulpfile.js
@@ -1,9 +1,9 @@
-'use strict'; 
+'use strict';
 
 var gulp = require('gulp');
 var casperjs = require('gulp-casperjs');
 
 gulp.task('integrate', function(){
-    gulp.src('./spec/integration/**')
-        .pipe(casperjs({command:'test'}));
+  gulp.src('./spec/integration/**')
+    .pipe(casperjs({command:'test'}));
 });
