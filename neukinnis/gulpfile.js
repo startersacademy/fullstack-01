e5bcaeac28c3bec8c6243e466ef175f21d3089e2
Trey Hunner <trey@treyhunner.com>
2015-01-14 19:31:31 -0800

    Start loopback server before running test

--- neukinnis/gulpfile.js
+++ neukinnis/gulpfile.js
@@ -2,8 +2,10 @@
 
 var gulp = require('gulp');
 var casperjs = require('gulp-casperjs');
+var server = require('./server/server.js');
 
 gulp.task('integrate', function(){
+  server.start();
   gulp.src('./spec/integration/**')
     .pipe(casperjs({command:'test'}));
 });
