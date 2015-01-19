5ea845bd7460d4bb3d2a269cd7a225b384754420
jstadragon <jstadragon@gmail.com>
2015-01-19 12:26:38 -0800

    add dependencies and jasmine test

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var gulp = require('gulp');
 var casperjs = require('gulp-casperjs');
 var spawn = require('child_process').spawn;
 var gutil = require('gulp-util');
+var run = require ('gulp-run');
+var server = require('gulp-develop-server');
+var gulpSequence = require('gulp-sequence');
 
 gulp.task('integrate', function(){
   gulp.src('./spec/integration/**')
@@ -22,3 +25,36 @@ gulp.task('integrate:win', function () {
   });
 
 });
+
+// Starts server for tasks that require a server
+gulp.task('server:start', function(cb){
+  server.listen({path: '.', delay: 3000}, cb);
+});
+
+// Stops server after tasks have run
+gulp.task('server:stop', function(cb){
+  server.kill();
+});
+
+// Runs api tests with frisbyjs and jasmine-node
+gulp.task('test:api', function (cb) {
+  console.log('running api tests');
+
+  var apiTestDir = 'spec/api/';
+
+  gulp.src(apiTestDir)
+    .pipe(run('jasmine-node ' + apiTestDir))
+      .on('data', function(){})
+      .on('end', cb)
+      .on('error', cb);
+});
+
+// Run all specified tests with gulpSequence
+gulp.task('test:win-all',
+  gulpSequence(
+  'server:start',
+  'test:api',
+  'test:integrate:win',
+  'server:stop'
+  )
+);
