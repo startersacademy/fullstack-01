a79ecd297eaf34a4d0d44eda1e2d1e647e9a28d5
jstadragon <jstadragon@gmail.com>
2015-01-20 01:32:34 -0800

    remove dependency of gulp-util

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,19 @@
 var gulp = require('gulp');
 var casperjs = require('gulp-casperjs');
 var spawn = require('child_process').spawn;
-var gutil = require('gulp-util');
 
-gulp.task('integrate', function(){
+gulp.task('integrate', function() {
   gulp.src('./spec/integration/**')
     .pipe(casperjs({command:'test'}));
 });
 
-gulp.task('integrate:win', function () {
+gulp.task('integrate:win', function() {
   var tests = ['./spec/integration'];
   var casperChild = spawn('casperjs.cmd', ['test'].concat(tests));
-  casperChild.stdout.on('data', function (data) {
-    gutil.log('CasperJS:', data.toString().slice(0, -1)); // Remove \n
+  casperChild.stdout.on('data', function(){
   });
 
-  casperChild.on('close', function (code) {
-    console.log("Test has finished!");
+  casperChild.on('close', function() {
+    console.log('Test has finished!');
   });
-
 });
