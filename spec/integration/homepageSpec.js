534b0c765f834efad8f0ff0f9c356b5b5a316678
jstadragon <jstadragon@gmail.com>
2015-01-21 15:46:19 -0800

    change link paths to correspond with name change

--- spec/integration/homepageSpec.js
+++ spec/integration/homepageSpec.js
@@ -8,8 +8,8 @@ casper.test.begin('homepage test', 6,
       test.assertTitle('Neukinnis', 'Page title good');
       test.assertExists('a[href="courses.html"]', 'courses link found');
       test.assertExists('a[href="instructors.html"]', 'instructors link found');
-      test.assertExists('a[href="about.html"]', 'about link found');
-      test.assertExists('a[href="contact.html"]', 'contact link found');
+      test.assertExists('a[href="aboutus.html"]', 'about link found');
+      test.assertExists('a[href="contactus.html"]', 'contact link found');
       test.assertExists('a[href="tel:524-469-4891"]', 'phonenumber link found and correct');
     });
     casper.run(function(){
