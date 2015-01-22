2b4ec7c74d326c84ee3b64662026f868a53d9c21
Trey Hunner <trey@treyhunner.com>
2015-01-21 19:04:24 -0800

    Make gulp handle server start and auto-restart

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,21 @@
 var gulp = require('gulp');
 var casperjs = require('gulp-casperjs');
 var spawn = require('child_process').spawn;
+var server = require('gulp-develop-server');
+
+gulp.task('server:start', function(cb){
+  server.listen({path: '.'}, cb);
+});
+
+gulp.task('server:stop', function(){
+  server.kill();
+});
 
 gulp.task('integrate', function(){
   gulp.src('./spec/integration/**')
     .pipe(casperjs({command:'test'}));
+});
+
+gulp.task('default', ['server:start'], function() {
+  gulp.watch(['./server/*.js']).on('change', server.changed);
 });
