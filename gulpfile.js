909266a83789444904b235ccef75b691bf6f975e
Trey Hunner <trey@treyhunner.com>
2015-01-21 19:04:57 -0800

    Make "gulp test" auto-start server

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var casperjs = require('gulp-casperjs');
 var spawn = require('child_process').spawn;
+var gulpSequence = require('gulp-sequence');
 var server = require('gulp-develop-server');
 
 gulp.task('server:start', function(cb){
@@ -13,10 +14,15 @@ gulp.task('server:stop', function(){
   server.kill();
 });
 
-gulp.task('integrate', function(){
-  gulp.src('./spec/integration/**')
-    .pipe(casperjs({command:'test'}));
+gulp.task('test:integration', function() {
+  return gulp.src('./spec/integration/**')
+    .pipe(casperjs({command: 'test'}));
 });
+
+gulp.task('test:all',
+  gulpSequence('server:start', 'test:integration', 'server:stop'));
+
+gulp.task('test', ['test:all']);
 
 gulp.task('default', ['server:start'], function() {
   gulp.watch(['./server/*.js']).on('change', server.changed);
