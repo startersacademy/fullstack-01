e81ef846ed676a6ee6c77b93884312eabade0d23
Trey Hunner <trey@treyhunner.com>
2015-01-21 19:26:49 -0800

    Use jasmine to run API tests

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var casperjs = require('gulp-casperjs');
 var spawn = require('child_process').spawn;
 var gulpSequence = require('gulp-sequence');
 var server = require('gulp-develop-server');
+var jasmine = require('gulp-jasmine');
 
 gulp.task('server:start', function(cb){
   server.listen({path: '.'}, cb);
@@ -19,8 +20,13 @@ gulp.task('test:integration', function() {
     .pipe(casperjs({command: 'test'}));
 });
 
+gulp.task('test:api', function(cb) {
+  return gulp.src('./spec/api/**')
+    .pipe(jasmine());
+});
+
 gulp.task('test:all',
-  gulpSequence('server:start', 'test:integration', 'server:stop'));
+  gulpSequence('server:start', 'test:api', 'test:integration', 'server:stop'));
 
 gulp.task('test', ['test:all']);
 
