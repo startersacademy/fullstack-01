dfc56a95cfe5fbc579c5a512e6b7f009ad79bc9a
Todd Bashor <jadedsurfer@gmail.com>
2015-01-21 16:12:19 -0800

    Move spa-index to the root of client

--- server/boot/spa.js
+++ server/boot/spa.js
@@ -7,15 +7,19 @@ module.exports = function mountApps(server) {
   var brfs = require('brfs');
 
   // Set up paths
-  var clientPath = path.join(__dirname, '../..', '/client/spa');
-  var clientRelativePath = '/client/spa';
+  var clientPath = path.join(__dirname, '../..', '/client/');
+  var clientRelativePath = '/client/';
 
 
   // Set up router
   var router = server.loopback.Router();
 
+  router.get('/index', function(req, res){
+    res.sendFile(path.join(clientPath, 'spa-index.html'));
+  });
+
   router.get('/js/build.js', browserify(
-    path.join(clientPath, 'js/main.js'),
+    path.join(clientPath, 'spa/js/main.js'),
     {
       transform: [ brfs ]
     }
@@ -28,6 +32,3 @@ module.exports = function mountApps(server) {
   server.use('/spa', router);
 
 };
-
-
-
