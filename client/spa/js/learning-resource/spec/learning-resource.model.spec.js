d9baf7a53cfedfb4d3f81489f1a569048485ff45
jstadragon <jstadragon@gmail.com>
2015-01-23 00:02:02 -0800

    initial commit for backbone model and first test

---
+++ client/spa/js/learning-resource/spec/learning-resource.model.spec.js
@@ -0,0 +1,27 @@
+/*
+global jasmine, describe, it, expect, beforeEach, afterEach, xdescribe, xit,
+spyOn
+*/
+
+// Get the code you want to test
+var Model = require('../learning-resource.model');
+
+// Test suite
+console.log('test learning.model');
+describe('Learning resource model', function(){
+
+  var model;
+
+  describe('when creating a new model', function(){
+
+    beforeEach(function(){
+      model = new Model();
+    });
+
+    it('sets the correct default values', function(){
+      expect(model.get('title')).toEqual('JavaScript Is Sexy');
+      expect(model.get('resourceType')).toEqual('link');
+      expect(model.get('description')).toEqual('Learn JavaScript properly');
+    });
+  });
+});
