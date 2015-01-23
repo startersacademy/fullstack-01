38d647968ce4865185973afa7846003442df7648
jstadragon <jstadragon@gmail.com>
2015-01-23 09:56:25 -0800

    removes commented out code

--- common/models/account.js
+++ common/models/account.js
@@ -1,6 +1,4 @@
 module.exports = function(account) {
-  // account.validatesPresenceOf('cust_name', {message: 'Cannot be blank'});
-  // account.validatesPresenceOf('account_type', {message: 'Cannot be blank'});
   account.validatesUniquenessOf('cust_name', {
     message: 'customer name is not unique'
   });
