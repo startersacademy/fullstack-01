e8b3b44ce77bc6d2cc284f6188a10b2d912ee3fd
Todd Bashor <jadedsurfer@gmail.com>
2015-01-25 15:38:13 -0800

    Enable the contact module and fix path to browserified build script for spa code

--- client/spa/js/main.js
+++ client/spa/js/main.js
@@ -3,9 +3,9 @@
 window.Backbone = require('./vendor').Backbone;
 
 // Include your code
-//var Contact = require('./contact/contact.controller');
+var Contact = require('./contact/contact.controller');
 // Initialize it
-//window.contact = new Contact({router:true, container: 'body'});
+window.contact = new Contact({router:true, container: 'body'});
 
 // Additional modules go here
 
