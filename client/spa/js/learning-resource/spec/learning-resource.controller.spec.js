07b6d10cf2a1a1ccc17bf8afaf46cb7bd6761da4
jstadragon <jstadragon@gmail.com>
2015-01-25 21:48:30 -0800

    fix properties in setting model

--- client/spa/js/learning-resource/spec/learning-resource.controller.spec.js
+++ client/spa/js/learning-resource/spec/learning-resource.controller.spec.js
@@ -4,13 +4,13 @@ spyOn
 */
 
 // Get the code you want to test
-var Controller = require('../contact.controller');
+var Controller = require('../learning-resource.controller');
 var $ = require('jquery');
 var matchers = require('jasmine-jquery-matchers');
 
 // Test suite
-console.log('test contact.controller');
-describe('Contact controller', function(){
+console.log('test learning-resource.controller');
+describe('Learning resource controller', function(){
 
   var controller;
 
@@ -26,7 +26,8 @@ describe('Contact controller', function(){
 
     it('has the expected routes', function(){
       expect(controller.routes).toEqual(jasmine.objectContaining({
-        'contacts/:id': 'showContact'
+        'learning-resource/:': 'list',
+        'learning-resource/:id': 'showLearningResource'
       }));
     });
 
@@ -40,30 +41,30 @@ describe('Contact controller', function(){
     });
   });
 
-  describe('when calling showContact', function(){
+  describe('when calling showLearningResource', function(){
 
     beforeEach(function(){
       jasmine.addMatchers(matchers);
     });
 
     var success = function(callbacks){
-      controller.model.set({'name': 'valid contact', 'contactType':'web'});
+      controller.model.set({'title': 'JavaScript Is Sexy', 'resourceType':'link', 'description': 'Learn JavaScript properly.'});
       callbacks.success(controller.model);
     };
     var err = function(callbacks){
       callbacks.error('error', controller.model);
     };
 
-    it('with a valid contact id, fetches the model', function(){
+    it('with a valid learning resource id, fetches the model', function(){
       spyOn(controller.model, 'fetch').and.callFake(success);
       var cb = function(err, view){
         expect(err).toBeNull();
-        expect(controller.model.get('name')).toEqual('valid contact');
+        expect(controller.model.get('title')).toEqual('JavaScript Is Sexy');
       };
-      controller.showContact(1, cb);
+      controller.showLearningResource(1, cb); //what is this 1
     });
 
-    it('with a valid contact id, renders the view', function(){
+    it('with a valid resource type, renders the view', function(){
       spyOn(controller.model, 'fetch').and.callFake(success);
       spyOn(controller.view, 'render').and.callFake(function(){
         controller.view.$el = 'fake render';
@@ -73,17 +74,17 @@ describe('Contact controller', function(){
         expect($('body')).toHaveText('fake render');
         expect(view.cid).toEqual(controller.view.cid);
       };
-      controller.showContact(1, cb);
+      controller.showLearningResource(1, cb);
     });
 
-    it('with an invalid contact id, renders an error message', function(){
+    it('with an invalid resource type, renders an error message', function(){
       spyOn(controller.model, 'fetch').and.callFake(err);
       var cb = function(err, view){
         expect(err).toBeTruthy();
         expect($('body')).toHaveText(
-          'There was a problem rendering this contact');
+          'There was a problem rendering this learning resource.');
       };
-      controller.showContact(1, cb);
+      controller.showLearningResource(1, cb);
     });
 
   });
