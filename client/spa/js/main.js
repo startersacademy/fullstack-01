264c34b527e43ffa51505ab637d6271d52866103
frances.go <goldlilys@goldlilys-media.com>
2015-02-08 15:54:36 -0800

    Update main routers to include Instructor collections

--- client/spa/js/main.js
+++ client/spa/js/main.js
@@ -3,10 +3,11 @@
 window.Backbone = require('./vendor').Backbone;
 
 // Include your code
-//var Contact = require('./contact/contact.controller');
+var Instructor = require('./instructor/instructor.controller');
+var Instructors = require('./instructor/instructors.controller');
 // Initialize it
-//window.contact = new Contact({router:true, container: 'body'});
-
+window.instructor = new Instructor({router:true, container: 'body'});
+window.instructors = new Instructors({router:true, container: 'body'});
 // Additional modules go here
 
 
