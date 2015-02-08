1c695c2fb33a3889d0a6f2731eebf66285624716
jstadragon <jstadragon@gmail.com>
2015-02-08 11:30:22 -0800

    remove stdio from windowsSpawn

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var server = require('gulp-develop-server');
 var jasmine = require('gulp-jasmine');
 var karma = require('karma').server;
 var jshint = require('gulp-jshint');
+var winSpawn = require('win-spawn');
 
 // Starts server for tasks that require a server
 gulp.task('server:start', function(done){
@@ -46,15 +47,23 @@ gulp.task('test:integration', function (done) {
     stopServer(1);
   };
 
+  var casperChild;
+
   if(isWindows){
     casperCmd = 'casperjs.cmd';
+    casperChild = winSpawn(casperCmd, ['test'].concat(tests),
+    {}, function(){
+      console.log('callback');
+    });
   }
-
-  var casperChild = spawn(casperCmd, ['test'].concat(tests),
+  else {
+    casperChild = spawn(casperCmd, ['test'].concat(tests),
     {stdio: 'inherit'});
+  }
 
   casperChild.on('close', function (code) {
     if (code) {
+      console.log('error');
       // If there is an error, format the error object for gulp
       // Gulp depends on the this structure
       var err = {
@@ -69,9 +78,67 @@ gulp.task('test:integration', function (done) {
       onError(err);
     } else {
       // Everything is good
+      console.log('close');
       done();
     }
   });
+  console.log('task end');
+});
+
+//integration1
+gulp.task('test:integration:alt1', function (done) {
+  var tests = ['spec/integration'];
+  var casperCmd = 'casperjs';
+  var casperChild;
+  var onError = function(code){
+    done(code);
+    // Gulp doesn't handle the error properly so clean up properly
+    stopServer(1);
+  };
+  var windowsSpawn = function(executable, args, options){
+    return spawn('cmd.exe',
+      ['/c', executable].concat(args), options);
+  };
+
+  if(process.platform === 'win32'){
+    casperChild = windowsSpawn(casperCmd, ['test'].concat(tests),
+      {});
+  } else {
+    casperChild = spawn(casperCmd, ['test'].concat(tests),
+      {stdio: 'inherit'});
+  }
+
+  // console.log(casperChild);
+  casperChild.stdout.on('data', function (data) {
+    console.log('CasperJS:', data.toString().slice(0, -1)); // Remove \n
+   });
+  casperChild.on('error', function(params){
+    console.log(params);
+  });
+
+  casperChild.on('close', function (code) {
+    if (code) {
+      console.log('close error');
+      // If there is an error, format the error object for gulp
+      // Gulp depends on the this structure
+      var err = {
+        err: {
+          msg: 'Integration test failure'
+        },
+        toString: function(){
+          return err.err.msg;
+        },
+        showStack: false
+      };
+      onError(err);
+    } else {
+      console.log('close');
+      // Everything is good
+      done();
+    }
+  });
+
+  console.log('task end');
 });
 
 // Runs api tests with frisbyjs and jasmine-node
@@ -125,7 +192,7 @@ gulp.task('test:all',
     'test:lint',
     'test:server-unit',
     'test:api',
-    'test:integration',
+    'test:integration:alt1',
     'server:stop'));
 
 // Shortcut to run the entire test suite
