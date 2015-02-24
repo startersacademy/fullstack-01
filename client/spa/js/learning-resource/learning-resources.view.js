386c21005116288736f3d321c6b469501dd01291
jstadragon <jstadragon@gmail.com>
2015-02-23 23:01:20 -0800

    add modal and create function for save and delete

--- client/spa/js/learning-resource/learning-resources.view.js
+++ client/spa/js/learning-resource/learning-resources.view.js
@@ -2,11 +2,35 @@
 
 var Backbone = require('../vendor/index').Backbone;
 var _ = require('../vendor/index')._;
+var $ = require('../vendor/index').$;
+var Model = require('./learning-resource.model');
 
 var fs = require('fs'); //will be replaced by brfs in the browser
 
 // readFileSync will be evaluated statically so errors can't be caught
 var template = fs.readFileSync(__dirname + '/learning-resources.html', 'utf8');
+
+var showMessage = function(type, msg) {
+  $('#msg').empty().addClass(type)
+    .html(msg).fadeIn().delay(2000)
+    .fadeOut('slow').queue(function(remove) {
+      $('#msg').removeClass(type);
+      remove();
+    });
+};
+
+var Add = Backbone.Model.extend({
+
+  urlRoot: '/learning-resources',
+
+  defaults: {
+    title: '',
+    resourceType: '',
+    authors: '',
+    description: ''
+  },
+
+});
 
 module.exports = Backbone.View.extend({
 
@@ -19,7 +43,9 @@ module.exports = Backbone.View.extend({
     'click .sortByTitle': 'sortByTitle',
     'click .sortByResourceType': 'sortByResourceType',
     'click .sortByAuthors': 'sortByAuthors',
-    'click .sortByDescription': 'sortByDescription'
+    'click .sortByDescription': 'sortByDescription',
+    'click .btn-lrs-add': 'renderModal',
+    'click .btn-lrs-del': 'delConfirm'
   },
 
   initialize: function() {
@@ -35,14 +61,10 @@ module.exports = Backbone.View.extend({
   },
 
   render: function() {
+    console.log(this.collection.models);
     var context = this.collection;
     this.$el.html(this.template(context));
     return this;
-  },
-
-  sortById: function(){
-    this.collection.trigger('sortById');
-    this.render();
   },
 
   sortByTitle: function(){
@@ -63,7 +85,110 @@ module.exports = Backbone.View.extend({
   sortByDescription: function(){
     this.collection.trigger('sortByDescription');
     this.render();
-  }
+  },
+
+  renderModal: function(){
+    var view = new ResourceDialog();
+    view.render();
+  },
+
+  delConfirm: function() {
+    var collection = this.collection;
+    var checkedIds = [];
+    if ($(':checkbox:checked').length === 0) {
+      showMessage('alert-info', 'No resources selected');
+    }
+    else {
+      $(':checkbox:checked').each(function () {
+        console.log($(this).val());
+        checkedIds.push($(this).val());
+      });
+      $.each(checkedIds, function(key, value) {
+        console.log(value);
+        var item = collection.get({id:value});
+        item.destroy();
+        collection.remove({id:value});
+      });
+    }
+  },
 
 });
 
+var ResourceDialog = Backbone.View.extend({
+  className: 'modal fade',
+  attributes: {
+    tabindex: '-1',
+    role: 'dialog',
+  },
+
+  initialize: function() {
+    this.model = this.model || new Add();
+    this.temp = _.template($('#dialog-template').html());
+  },
+
+  events: {
+    'click #lrs-add-save': 'saveNewResource',
+    'click .close': 'close'
+  },
+
+  render: function() {
+    var context = this.model.toJSON();
+    this.$el.html(this.temp(context)).appendTo(document.body);
+    this.$el.modal();
+    return this;
+  },
+
+  close: function() {
+    this.remove();
+  },
+
+  saveNewResource: function(){
+
+    var newResource = new Model();
+
+    var authorsFormat = function(authored) {
+      var authors = [];
+      // console.log($('#auth').val().split(','));
+      // if ($('#lrs-add-authors').val() === '') authors = null;
+      // else {
+      //   $.each($('#lrs-add-authors').val().split(','), function(key,value){
+      //     console.log(value);
+      //     authors.push(value.trim());
+      //   });
+      // }
+      authored(authors);
+    };
+
+    var saveArgs = {
+      attributes: {
+        title: $('#lrs-add-title').val().trim(),
+        resourceType: $('#lrs-add-resourceType option:selected').val(),
+        description: $('#lrs-add-description').val().trim(),
+        authors: $('#lrs-add-authors').val().split(',')
+      },
+      options: {
+        success: function(response){
+          $('#lrs-dismiss').click();
+          setTimeout(function() {
+            // collection.fetch();
+            // collection.add(saveArgs.attributes);
+            $('table tr td').find('[value="na"]').attr('value', response.id);
+          }, 400);
+
+          showMessage('alert-success', 'Successfully updated');
+          console.log('success');
+        },
+        error: function(model, error){
+          //server response errors if no validations specified
+        }
+      }
+    };
+
+    newResource.save(saveArgs.attributes, saveArgs.options);
+
+    // if (newResource.validationError) {
+    //   showMessage('alert-danger', this.model.validationError);
+    // }
+  },
+
+});
